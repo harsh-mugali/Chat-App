47cc56a00ef2c59490500c0873bfa2b898d8c110
harsh-mugali <mugaliharsh@gmail.com>
2025-12-27 16:57:50 +0530

    Add authentication routes and controller methods for signup, login, and logout

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const signup = (req, res) => {
+    console.log('Signup Route');
+};
+export const login = (req, res) => {
+    console.log('Login Route');
+};
+export const logout = (req, res) => {
+    console.log('Logout Route');
+};
