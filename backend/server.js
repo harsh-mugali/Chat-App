fc06af1f5efe392b680c7597e1dd957b971a9543
harsh-mugali <mugaliharsh@gmail.com>
2025-12-28 22:39:19 +0530

    Add cookie parser middleware and integrate message routes into the server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv';
-
+import cookieParser from 'cookie-parser';
 import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 
 const app = express();
@@ -10,11 +11,11 @@ dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 app.use(express.json());   // to parse the body of the request
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+app.use(cookieParser());
+
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 app.listen(PORT, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${PORT}`);
