e3ecb03aae1d5578143568606da5ebe76d985faf
harsh-mugali <mugaliharsh@gmail.com>
2026-01-03 00:26:33 +0530

    fix: resolve Express wildcard route crash on Node 20

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
@@ -10,9 +11,13 @@ import userRoutes from './routes/user.routes.js';
 import connectToMongoDB from './db/connectToMongoDB.js';
 import { app, server } from './socket/socket.js';
 
+
 dotenv.config();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 5000;
+
 
 app.use(express.json());   // to parse the body of the request
 app.use(cookieParser());
@@ -22,6 +27,15 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+// app.get("/*", (req, res) => {
+// 	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+// });
+
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
